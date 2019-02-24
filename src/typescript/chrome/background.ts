597c4393b27ca72f9635cbcb0653b2ce127567c6
Miguel <miguel@miguelpz.com>
2019-02-24 23:53:59 +0000

    Fix code style

--- src/typescript/chrome/background.ts
+++ src/typescript/chrome/background.ts
@@ -1,26 +1,24 @@
 import "chrome"
 
-chrome.runtime.onInstalled.addListener(details => {
-    let tabId: number | undefined;
+chrome.runtime.onInstalled.addListener(() => {
+  let tabId: number | undefined;
 
-    chrome.tabs.onRemoved.addListener((id, info) => {
-        if (id === tabId) {
-            tabId = undefined
-        }
-    });
+  chrome.tabs.onRemoved.addListener((id) => {
+    if (id === tabId) {
+      tabId = undefined
+    }
+  });
 
-    // create a new tab or make already created tab active
-    chrome.browserAction.onClicked.addListener(tabs => {
-        let url: string = chrome.extension.getURL('index.html')
+  // create a new tab or make already created tab active
+  chrome.browserAction.onClicked.addListener(() => {
+    let url: string = chrome.extension.getURL('index.html')
 
-        if (!tabId) {
-            chrome.tabs.create({ url: url }, tab => {
-                tabId = tab.id
-            });
-        } else {
-            chrome.tabs.update(tabId, { active: true })
-        }
-    });
+    if (!tabId) {
+      chrome.tabs.create({ url: url }, tab => {
+        tabId = tab.id
+      });
+    } else {
+      chrome.tabs.update(tabId, { active: true })
+    }
+  });
 })
-
-
